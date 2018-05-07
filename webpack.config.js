6a55b98e327f749681785ac143d34df6c46e17f2
Maico Borges <maicoqb@gmail.com>
2018-05-06 22:08:00 -0300

    Configurando build do projeto de front.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     entry: './front/src/main.js',
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, './public/dist'),
         publicPath: '/dist/',
         filename: 'build.js'
     },
